c6ce487c1f1960f0346128b376161acc7696cb15
Raul <raulferrodrigues@gmail.com>
2019-08-02 16:23:11 -0300

    hero, move, jump

---
+++ GravityRush/MoveComponent.swift
@@ -0,0 +1,35 @@
+//
+//  MoveComponent.swift
+//  GravityRush
+//
+//  Created by Raul Rodrigues on 8/2/19.
+//  Copyright © 2019 Luiz Felipe Trindade. All rights reserved.
+//
+
+import Foundation
+import GameplayKit
+
+final class MoveComponent: GKComponent {
+    let node: SKNode
+    let timeOffset: TimeInterval
+    
+    init(node: SKNode, timeOffset: TimeInterval) {
+        self.node = node
+        self.timeOffset = timeOffset
+        super.init()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("do not use decoders")
+    }
+    
+    func move(timeOffset: TimeInterval, direction: String) {
+        if direction == "left" {
+            node.position.x += CGFloat(80 * timeOffset)
+        } else if direction == "right" {
+            node.position.x -= CGFloat(80 * timeOffset)
+        } else {
+            fatalError("Direction is wrong")
+        }
+    }
+}
