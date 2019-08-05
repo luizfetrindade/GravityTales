7262f5e33636138c9d95a05b020290f82a811662
Raul <raulferrodrigues@gmail.com>
2019-08-05 16:29:24 -0300

    new stuff

---
+++ GravityRush/Game/TestScene.swift
@@ -0,0 +1,15 @@
+//
+//  TestScene.swift
+//  GravityRush
+//
+//  Created by Raul Rodrigues on 8/5/19.
+//  Copyright © 2019 Luiz Felipe Trindade. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+import CoreMotion
+
+
+class TestScene: SKScene {
+}
